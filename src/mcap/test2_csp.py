499ef03a3d3b7e2700bbd36a49ce6aa1e250fb26
Elvis Pfützenreuter <epx@signove.com>
2010-06-24 12:37:15 -0300

    Added err/drift statistics to CSP test script

--- src/mcap/test2_csp.py
+++ src/mcap/test2_csp.py
@@ -40,7 +40,7 @@ class MyInstance(MCAPInstance):
 
 	def SyncCapabilitiesResponse(self, mcl, err, btclockres, synclead,
 					tmstampres, tmstampacc):
-		print "CSP Caps resp %s btres=%d lead=%d tsres=%d tsacc=%d" % \
+		print "CSP Caps resp %s btres %d lead %d tsres %d tsacc %d" % \
 			(err and "Err" or "Ok", btclockres,
 				synclead, tmstampres, tmstampacc)
 		if err:
@@ -52,20 +52,43 @@ class MyInstance(MCAPInstance):
 
 		# resets timestamp in 1s
 		btclock = btclock[0] + 3200
+		# begins with a timestamp of 5 full seconds
+		initial_tmstamp = 5000000
 
-		instance.SyncSet(mcl, True, btclock, 5000000)
+		mcl._it = initial_tmstamp
+		mcl._ib = btclock
+		mcl._iema = None
+
+		instance.SyncSet(mcl, True, btclock, initial_tmstamp)
 	
 	def SyncSetResponse(self, mcl, err, btclock, tmstamp, tmstampacc):
-		print "CSP Set resp: %s btclk=%d ts=%d tsacc=%d" % \
+		print "CSP Set resp: %s btclk %d ts %d tsacc %d" % \
 			(err and "Err" or "Ok", btclock,
 				tmstamp, tmstampacc)
 		if err:
 			self.bye()
 
 	def SyncInfoIndication(self, mcl, btclock, tmstamp, accuracy):
-		print "CSP Indication btclk=%d ts=%d tsacc=%d" % \
+		print "CSP Indication btclk %d ts %d tsacc %d" % \
 			(btclock, tmstamp, accuracy)
-		# FIXME calculate drift, deviation
+		self.calc_drift(mcl, btclock, tmstamp)
+
+	def calc_drift(self, mcl, btclock, tmstamp):
+		btdiff = mcl.sm.csp.btdiff(mcl._ib, btclock)
+		btdiff *= 312.5
+		tmdiff = tmstamp - mcl._it
+		err = tmdiff - btdiff
+
+		if mcl._iema is None:
+			errma = mcl._iema = err
+		else:
+			last_ma = mcl._iema
+			errma = mcl._iema = 0.05 * err + 0.95 * mcl._iema
+
+		drift = float(errma) / (float(tmdiff) / 1000000)
+		
+		print "\terror %dus moving avg %dus drift %dus/h" % \
+			(err, errma, drift * 3600)
 
 try:
 	remote_addr = (sys.argv[1], int(sys.argv[2]))
