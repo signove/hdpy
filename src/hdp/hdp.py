10e391acd0a3447dbc4df56fd72682ad2671be12
Elvis Pfützenreuter <epx@signove.com>
2010-07-10 01:13:33 -0300

    Updated HDP API skeleton

--- src/hdp/hdp.py
+++ src/hdp/hdp.py
@@ -18,77 +18,65 @@ sys.path.insert(0, "..")
 from mcap.mcap_instance import MCAPInstance
 
 
-class Adapter(object):
-	def __init__(self, addr, devid):
-		self.addr = addr
-		self.devid = devid
+class HealthManager(object):
+	def __init__(self):
+		self.applications = []
+		pass
 
-	def CreateInstance(self, config):
-		return HDPInstance(self.addr, config)
+	def RegisterApplication(self, agent, config): # -> HealthApplication
+		'''
+		{
+		  "end_points" : [{ (optional)
+			"agent": path,  (mapped with HealthAgent)
+			"role" : ("source" or "sink"), (mandatory)
+			"specs" :[{ (mandatory)
+				"data_type" : uint16, (mandatory)
+				"description" : string, (optional)
+			}]
+		  }]
+		}
+		'''
+		application = HealthApplication(agent, config)
+		if not application:
+			return None
+		self.applications.append(application)
+		return application
 
-	def RegisterAgent(self, instance, agent):
-		instance.set_observer(agent)
+	def UnregisterApplication(self, application):
+		application.stop()
+		self.applications.remove(application)
 
-	def CloseInstance(self, instance):
-		instance.stop()
+	def UpdateServices(self):
+		# FIXME searches remote devices
+		pass
 
+	def ServiceDiscovered(self, service):
+		print "HealthManager.ServiceDiscovered not overridden"
 
-DefaultAdapter = Adapter("00:00:00:00:00:00", -1)
-
-
-def GetAdapterList():
-	adapters = [DefaultAdapter]
-	# FIXME get the rest
-
-
-def RegisterAgent(instance, agent):
-	instance.set_observer(agent)
+	def ServiceRemoved(self, service):
+		print "HealthManager.ServiceRemoved not overridden"
 
 
-def CloseInstance(instance):
-	instance.stop()
-
-
-class HDPInstance(MCAPInstance):
-
-	def __init__(self, adapter, config):
-		self.adapter = adapter
-		self.listener = not not config
-		self.__init__(self.adapter, self.listener)
-		self.observer = HealthAgent()
-
-		# FIXME link list, link list housekeeping
-
-		if not config:
-			return		
-
-		# FIXME
+class HealthApplication(MCAPInstance):
+	def __init__(self, agent, config):
+		pass
+		'''
+		{
+		  "end_points" : [{ (optional)
+			"agent": path,  (mapped with HealthAgent)
+			"role" : ("source" or "sink"), (mandatory)
+			"specs" :[{ (mandatory)
+				"data_type" : uint16, (mandatory)
+				"description" : string, (optional)
+			}]
+		  }]
+		}
+		'''
 		"""
-                Dict is defined as bellow:
-                { "data_spec" : The data_spec is the data exchange specification
-                                (see section 5.2.10 of the specification
-                                document) possible values:
-                                        0x00 = reserved,
-                                        0x01 [IEEE 11073-20601],
-                                        0x02..0xff reserved,
-                                (optional)
-                  "end_points" : [{ (optional)
-                        "mdepid" : uint8, (optional)
-                        "role" : ("source" or "sink"), (mandatory)
-                        "specs" :[{ (mandatory)
-                                "data_type" : uint16, (mandatory)
-                                "description" : string, (optional)
-                        }]
-                  }]
-                }
-
+		FIXME chk
+		FIXME null config
 		FIXME publish sdp record
 		"""
-
-	def set_observer(self, observer):
-		if not isinstance(observer, HealthAgent):
-			raise Exception("Observer needs to be HealthAgent")
-		self.observer = observer
 
 	def CreateMCL(self, addr, dpsm):
 		# FIXME
@@ -181,7 +169,6 @@ class HDPInstance(MCAPInstance):
 		# FIXME
 		pass
 	def SyncCapabilitiesResponse(self, mcl, err, btclockres, synclead,
-		# FIXME
 				tmstampres, tmstampacc):
 		pass
 	def SyncSetResponse(self, mcl, err, btclock, tmstamp, tmstampacc):
@@ -192,143 +179,97 @@ class HDPInstance(MCAPInstance):
 		pass
 
 
-def GetDeviceList():
-	# FIXME
-	pass
-
-
-def CheckDevice(addr):
-	# FIXME
-	pass
-
-
-def AddDeviceManually(addr, config):
-	# FIXME
-	pass
-
-
-class HealthDevice(object):
+class HealthService(object):
 	def __init__(self, addr):
 		self.addr = addr
 
-	def GetHealthInstances(self):
-		# FIXME
+	def Echo(self, data, reply_handler, error_handler):
 		pass
 		"""
-		Gets the information of the remote instances present in this
-		device and published on its SDP record. The returned data
-		follows this format.
-
-		[{"id": uint32,
-		  "data_spec" : data spec,
-		  "end_points":
-			["mdepid": uint8,
-			 "role"  : "source" or "sink" ,
-			 "specs" : [{
-				"dtype"       : uint16,
-				"description" : string, (optional)
-			 }]
-			]
-		}];
-		"""
-
-	def Connect(self, local_instance, remote_instance, cb, *args): # -> HealthLink
-		# FIXME
-		pass
-		"""
-		FIXME demands 'null' (no-listening) instance, created on demand
-		"""
-
-	def Disconnect(self, link):
-		# FIXME
-		pass
-
-
-class HealthLink(object):
-	def __init__(self, mcl):
-		self.mcl = mcl
-
-	def echo(self, string, cb, *args):
-		# FIXME
-		pass
-		"""
-		Sends an echo petition to the remote intance. Returns True if
+		Sends an echo petition to the remote service. Returns True if
 		response matches with the buffer sent. If some error is detected
 		False value is returned and the associated MCL is closed.
-
-		Uses MDEP ID 00, connects MDL and disconnects.
 		"""
 
-	def OpenDataChannel(self, mdepid, config, cb, *args): # -> mdl id
-		# FIXME
-		pass
+	def OpenDataChannel(end_point, conf, reply_handler, error_handler):
 		"""
 		Creates a new data channel with the indicated config to the
 		remote MCAP Data End Point (MDEP).
 		The configuration should indicate the channel quality of
-		service. In the current version of HDP, valid values are 0x01
-		for reliable channels and 0x02 for streaming data channel.
+		service using one of this values "Reliable", "Streaming", "Any".
+
+		Returns an string that identifies the data channel.
+		Possible errors: org.bluez.Error.InvalidArguments
+				org.bluez.Error.HealthError
 		"""
-
-	def ReconnectDataChannel(self, mdlid, cb, *args):
-		# FIXME
-		pass
-
-	def GetDataChannelFileDescriptor(self, mdlid):
-		# FIXME
-		pass
-
-	def DeleteDataChannel(self, mdlid, cb, *args):
-		# FIXME
 		pass
 
 	def DeleteAllDataChannels(self):
-		# FIXME
-		pass
+		"""
+		Deletes all data channels so they will not be available for
+		future use.
+		"""
 
-	def GetDataChannelStatus(self):
-		# FIXME
+	def GetProperties(self):
 		pass
 		"""
-		{
-			"reliable": [mdlid_r1, mdlid_r2, ...],
-			"streaming" : [mdlid_s1, mdlid_s2, ...]
+		EndPoints array of {
+			"end_point": string,
+			"role"  : "source" or "sink" ,
+			"specs" : [{
+				"dtype"       : uint16,
+				"description" : string, (optional)
+				}]
 		}
-
 		"""
 
 
-class HealthAgent(object):
-	'''
-	Abstract class for health agents that observe a given instance.
-	'''
-
+class HealthDataChannel(object):
 	def __init__(self):
 		pass
 
-	def LinkConnected(self, link):
-		print "HealthAgent.LinkConnected not overridden"
+	def GetProperties(self):
+		"""
+		{"QualityOfService": string}
+		{"Connected": boolean}
+		"""
 
-	def LinkPaused(self, link):
-		# closed
-		print "HealthAgent.LinkPaused not overridden"
-	
-	def LinkResumed(self, link):
-		print "HealthAgent.LinkResumed not overridden"
+	def Acquire(self):
+		"""
+		Returns the file descriptor for the data channel_ZZ
+		"""
+		pass
 
-	def LinkDisconnected(self, link):
-		# deleted
-		print "HealthAgent.LinkDisconnected not overridden"
+	def Release(self):
+		pass
 
-	def CreatedDataChannel(self, link, mdlid, conf):
-		# conf: (0x01 reliable, 0x02 streaming).
-		print "HealthAgent.CreatedDataChannel not overridden"
+	def Delete(self):
+		pass
 
-	def DataChannelReconnected(self, link, mdlid, conf):
-		print "HealthAgent.DataChannelReconnected not overridden"
+	def Reconnect(self, reply_handler, error_handler):
+		pass
 
-	def DeletedDataChannel(self, link, mdlid):
-		print "HealthAgent.DeletedDataChannel not overridden"
 
-	def DeletedAllDataChannels(self, link):
-		print "HealthAgent.DeletedAllDataChannels not overridden"
+class HealthApplicationAgent(object):
+	def __init__(self):
+		pass
+
+	def Release(self):
+		pass
+
+	def ServiceDiscovered(service):
+		print "HealthApplicationAgent.ServiceDiscovered not overridden"
+
+	def DataChannelRemoved(service, data_channel):
+		print "HealthApplicationAgent.DataChannelRemoved not overridden"
+
+
+def HealthEndPointAgent(object):
+	def __init__(self):
+		pass
+
+	def Release(self):
+		pass
+
+	def DataChannelCreated(self, data_channel, reconn):
+		pass
