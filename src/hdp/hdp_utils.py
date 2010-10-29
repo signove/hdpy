fa7131bcb7fefddd8b2da2153d67344a710eb4c2
Jose Luis do Nascimento <jose.nascimento@signove.com>
2010-10-29 15:57:36 -0300

    Added missing header files

--- src/hdp/hdp_utils.py
+++ src/hdp/hdp_utils.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8
+
+################################################################
+#
+# Copyright (c) 2010 Signove. All rights reserved.
+# See the COPYING file for licensing details.
+#
+# Autors: Elvis Pfützenreuter < epx at signove dot com >
+#         Raul Herbster < raul dot herbster at signove dot com >
+################################################################
+
 def s2b(msg):
 	if msg is None:
 		return None
@@ -9,7 +20,7 @@ def b2s(msg):
 	return "".join([chr(int(x)) for x in msg])
 
 def test():
-	s = chr(0xe2)+"ABCDE\0\1\2"
+	s = chr(0xe2) + "ABCDE\0\1\2"
 	b = [0xe2, 65, 66, 67, 68, 69, 0, 1, 2]
 	assert(b == s2b(s))
 	assert(s == b2s(b))
--- src/mcap/__init__.py
+++ src/mcap/__init__.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8
+
+################################################################
+#
+# Copyright (c) 2010 Signove. All rights reserved.
+# See the COPYING file for licensing details.
+#
+# Autors: Elvis Pfützenreuter < epx at signove dot com >
+#         Raul Herbster < raul dot herbster at signove dot com >
+################################################################
+
 __all__ = ["mcap_sock", "mcap_loop", "mcap", "mcap_csp", "mcap_instance"]
