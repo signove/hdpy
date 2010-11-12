c9ed8f38d296ceec182753910a8a95d8178ceeca
Elvis Pfützenreuter <epx@signove.com>
2010-11-12 13:50:08 -0200

    Added MCAP PTS automation scripts

---
+++ scripts/bluez/pts/mcap_suite.py
@@ -0,0 +1,381 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import gobject
+import dbus.mainloop.glib
+import glib
+import string
+from test_suite import TestSuite, mcap_suites
+import subprocess
+
+mcap_iface = "org.bluez.mcap"
+bdaddr = "00:80:98:E7:36:9F"
+cpsm = 0x1001
+dpsm = 0x1003
+mdepid = 1
+conf = 0x00
+hci_interface = None
+
+print "Bdaddr", bdaddr
+session = None
+mcl = None
+csp_caps = None
+last_mdl = None
+
+mcap_tests = TestSuite(mcap_suites)
+
+def object_signal(*args, **kwargs):
+    global mcl
+    global last_mdl
+    if 'member' not in kwargs:
+        return
+    sig_name = kwargs['member']
+    if sig_name == "Recv":
+        mdl, data = args
+        print "Received data in mdl", mdl
+        print "Data:", data
+    elif sig_name == "MCLConnected":
+        tmp_mcl, _ = args
+        if mcl != None:
+            print "Mcl is already connected"
+            return
+        print "New mcl"
+        mcl = tmp_mcl
+    elif sig_name == "MDLConnected":
+        tmp_mcl, mdl = args
+        if mcl != tmp_mcl:
+            return
+        last_mdl = mdl
+        print "New mdl connected", mdl
+    elif sig_name == "MDLReconnected":
+        mdl = args[0]
+        last_mdl = mdl
+        print "New mdl reconnected", mdl
+    elif sig_name == "MCLDisconnected":
+        tmp_mcl = args[0]
+        if mcl != tmp_mcl:
+            return
+        print "mcl disconnected"
+    elif sig_name == "MCLReconnected":
+        tmp_mcl = args[0]
+        if mcl != tmp_mcl:
+            return
+        print "mcl reconnected"
+    elif sig_name == "MDLDeleted":
+        mdl = args[0]
+        last_mdl = None
+        print "mdl deleted", mdl
+    elif sig_name == "MDLAborted":
+        tmp_mcl, mdl = args
+        if mcl != tmp_mcl:
+            return
+        print "mdl aborted", mdl
+
+def adapter_signal(value, path, interface, member):
+    hci = str(value)
+    if hci.split("/")[-1] == ad.split("/")[-1]:
+        print "Adapter went out, quitting"
+        mainloop.quit()
+
+def con_mcl(cmd):
+    global mcl
+    if session == None:
+        print "Session not connected"
+        return
+    print "connect_mcl"
+    try:
+        mcl = mcap.CreateMCL(session, bdaddr, cpsm)
+    except Exception, e:
+        print e
+
+def close_mcl(cmd):
+    global mcl
+    if mcl == None:
+        print "mcl is not connected"
+        return
+    print "close_mcl"
+    try:
+        mcap.CloseMCL(mcl)
+    except Exception, e:
+        print e
+
+def delete_mcl(cmd):
+    global mcl
+    if mcl == None:
+        print "mcl is not connected"
+        return
+    print "delete_mcl"
+    try:
+        mcap.DeleteMCL(mcl)
+        mcl = None
+    except Exception, e:
+        print e
+
+def send_data(cmd):
+    global last_mdl
+    if last_mdl:
+        data = "test"
+        try:
+            mcap.Send(last_mdl, data)
+        except Exception, e:
+            print "Error", e
+
+def con_mdl(cmd):
+    global last_mdl
+    print "Connect mdl"
+    try:
+        mdl = mcap.CreateMDL(mcl, mdepid, conf)
+        mcap.ConnectMDL(mdl, dpsm)
+        last_mdl = mdl
+        print "connected mdl", mdl
+    except Exception, e:
+        print e
+
+def close_mdl(cmd):
+    global last_mdl
+    print "Closing data channel", last_mdl
+    if last_mdl:
+        try:
+            mcap.CloseMDL(last_mdl)
+        except Exception, e:
+            print "Error", e
+
+def recon_mdl(cmd):
+    global last_mdl
+    print "Reconnecting mdl", last_mdl
+    if last_mdl:
+        try:
+            mcap.ReconnectMDL(last_mdl)
+            mcap.ConnectMDL(last_mdl, dpsm)
+            print "reconnected mdl", last_mdl
+        except Exception, e:
+            print e
+
+def del_mdl(cmd):
+    global last_mdl
+    print "Deleting mdl", last_mdl
+    if last_mdl:
+        try:
+            mcap.DeleteMDL(last_mdl)
+            print "deleted mdl", last_mdl
+        except Exception, e:
+            print e
+
+def del_all(cmd):
+    global mcl
+    global last_mdl
+    if mcl == None:
+        print "Mcl is not connected"
+        return
+    print "Deleting all mdls"
+    try:
+        last_mdl = None
+        mcap.DeleteAll(mcl)
+        pass
+    except Exception, e:
+        print e
+
+def abort_mdl(cmd):
+    global last_mdl
+    print "Aborting mdl", last_mdl
+    try:
+        mcap.AbortMDL(last_mdl)
+        print "aborted mdl", last_mdl
+    except Exception, e:
+        print e
+
+def neg_mdl(cmd):
+    global last_mdl
+    print "Negotiating mdl"
+    try:
+        mdl = mcap.CreateMDL(mcl, mdepid, conf)
+        last_mdl = mdl
+        print "negotitated mdl", mdl
+    except Exception, e:
+        print e
+
+def csp_cap(cmd):
+    global csp_caps
+    print "Requesting CSP capabilities"
+    try:
+        ppm = 50
+        csp_caps = mcap.SyncCapabilities(mcl, ppm)
+        print "Capabilities:", csp_caps
+    except Exception, e:
+        print e
+
+def csp_set(cmd):
+    print "Setting CSP timestamp"
+    try:
+        send_indication = 0
+        retries = 5
+        btclock = 0xffffffff
+        while retries > 0 and btclock == 0xffffffff:
+            print "Reading BT clock"
+            btclock = mcap.SyncBtClock(mcl)
+            retries -= 1
+        print "\tBT clock is 0x%x" % btclock
+        btclock += 3200 * 10 # PTS wants it fairly into the future
+        timestamp = 0x1122334455667788
+        print "\tScheduled BT clock will be 0x%x" % btclock
+        print mcap.SyncSet(mcl, send_indication, btclock, timestamp)
+    except Exception, e:
+        print e
+
+def csp_seti(cmd):
+    print "Setting CSP timestamp immediately"
+    try:
+        send_indication = 0
+        btclock = 0xffffffff
+        print "\tBT clock is 0x%x (immediate)" % btclock
+        timestamp = 0x1122334455667788
+        print mcap.SyncSet(mcl, send_indication, btclock, timestamp)
+    except Exception, e:
+        print e
+
+def csp_set_ind(cmd):
+    print "Setting CSP timestamp"
+    try:
+        send_indication = 1
+        retries = 5
+        btclock = 0xffffffff
+        while retries > 0 and btclock == 0xffffffff:
+            print "Reading BT clock"
+            btclock = mcap.SyncBtClock(mcl)
+            retries -= 1
+        print "\tBT clock is 0x%x" % btclock
+        btclock += 3200 * 10 # PTS wants it fairly into the future
+        timestamp = 0x1122334455667788
+        print mcap.SyncSet(mcl, send_indication, btclock, timestamp)
+    except Exception, e:
+        print e
+
+def enable_csp(cmd):
+    print "Enabling CSP"
+    try:
+        print mcap.SyncEnable(session) and "Enabled" or "Not enabled"
+    except Exception, e:
+        print e
+
+def disable_csp(cmd):
+    print "Disabling CSP"
+    try:
+        print mcap.SyncDisable(session) and "Disabled" or "Not disabled"
+    except Exception, e:
+        print e
+
+def bt_up(cmd):
+    global hci_interface
+    print "Enabling Bluetooth"
+    print "Status: %d" % (subprocess.call(["sudo", "hciconfig", hci_interface, "up"]))
+
+def bt_down(cmd):
+    global hci_interface
+    print "Disabling Bluetooth"
+    print "Status: %d" % (subprocess.call(["sudo", "hciconfig", hci_interface, "down"]))
+
+
+commands = {"con_mcl": {"help":"con_mcl", "npar": 0, "fun": con_mcl},
+    "close_mcl": {"help":"close_mcl", "npar": 0, "fun": close_mcl},
+    "delete_mcl": {"help":"delete_mcl", "npar": 0, "fun": delete_mcl},
+    "send_data": {"help":"send_data mdl", "npar": 0, "fun": send_data},
+    "con_dc": {"help":"con_dc", "npar": 0, "fun": con_mdl},
+    "close_dc": {"help":"close_dc mdl", "npar": 0, "fun": close_mdl},
+    "recon_dc": {"help":"recon_dc mdl", "npar": 0, "fun": recon_mdl},
+    "del_dc": {"help":"del_dc mdl", "npar": 0, "fun": del_mdl},
+    "del_all": {"help":"del_all", "npar": 0, "fun": del_all},
+    "abort_dc": {"help":"abort_dc mdl", "npar": 0, "fun": abort_mdl},
+    "neg_dc": {"help":"neg_dc", "npar": 0, "fun": neg_mdl},
+    "enable_csp": {"help":"enable_csp", "npar": 0, "fun": enable_csp},
+    "disable_csp": {"help":"disable_csp", "npar": 0, "fun": disable_csp},
+    "csp_cap": {"help":"csp_cap", "npar": 0, "fun": csp_cap},
+    "csp_set": {"help":"csp_set", "npar": 0, "fun": csp_set},
+    "csp_seti": {"help":"csp_seti", "npar": 0, "fun": csp_seti},
+    "csp_set_ind": {"help":"csp_set_ind", "npar": 0, "fun": csp_set_ind},
+    "bt_down": {"help":"bt_down", "npar": 0, "fun": bt_down},
+    "bt_up": {"help":"bt_up", "npar": 0, "fun": bt_up},
+    "exit": {"help":"exit", "npar": 0, "fun": exit},
+    }
+
+def check_cmd(cmd):
+    try:
+        if cmd[0].lower() not in commands.keys():
+            return False
+        c = commands[cmd[0].lower()]
+        if (c["npar"] + 1) == len(cmd):
+            return True
+        print "help: %s" % c["help"]
+    except:
+        pass
+    return False
+
+def stdin_cb(fd, condition):
+    global mcap_tests
+
+    string.split(fd.readline()) # Read enter key
+    cmd = mcap_tests.get_current_command()
+
+    if not check_cmd(cmd):
+        print "Available commands"
+        for c in commands:
+            print "\t%s" % commands[c]["help"]
+        return True
+    commands[cmd[0].lower()]["fun"](cmd)
+
+    mcap_tests.next_command()
+    mcap_tests.command_info()
+    return True
+
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+bus = dbus.SystemBus()
+
+manager = dbus.Interface(bus.get_object("org.bluez", "/"), "org.bluez.Manager")
+
+ad = manager.DefaultAdapter()
+print "Binding to adapter", ad
+hci_interface = ad.split("/")[-1]
+
+mcap = dbus.Interface(bus.get_object("org.bluez", ad), mcap_iface)
+
+bus.add_signal_receiver(object_signal, bus_name="org.bluez",
+                member_keyword="member",
+                path_keyword="path",
+                interface_keyword="interface",
+                dbus_interface=mcap_iface,
+                byte_arrays=True)
+
+bus.add_signal_receiver(adapter_signal, bus_name="org.bluez",
+                signal_name="AdapterAdded",
+                path_keyword="path",
+                member_keyword="member",
+                interface_keyword="interface")
+
+bus.add_signal_receiver(adapter_signal, bus_name="org.bluez",
+                signal_name="AdapterRemoved",
+                path_keyword="path",
+                member_keyword="member",
+                interface_keyword="interface")
+
+session = mcap.StartSession(cpsm, dpsm)
+
+
+try:
+    print "Starting tests Session: ", session
+    print 'Press Enter to send commands'
+    mcap_tests.command_info()
+
+    glib.io_add_watch(sys.stdin, glib.IO_IN, stdin_cb)
+
+    mainloop = gobject.MainLoop()
+    mainloop.run()
+finally:
+    try:
+        mcap.StopSession(session)
+        print
+        print "Stopped instance, thanks"
+        print
+    except:
+        pass
+
---
+++ scripts/bluez/pts/test_suite.py
@@ -0,0 +1,155 @@
+
+mcap_suites = {
+                "01_CM": {
+                          "CE_BV_01_C": ["con_mcl", "con_dc"], #TODO: Failed with Invalid MDL configuration
+                          "CE_BV_02_C": ["send_data"],
+                          "CE_BV_03_C": ["con_mcl", "send_data"],
+                          "CE_BV_04_C": ["con_dc"] #TODO: Failed with Invalid MDL configuration
+                          },
+                "02_CM_DIS": {
+                              "CM_DIS_01_C": ["close_mcl"],
+                              "CM_DIS_02_C": [],
+                              "CM_DIS_03_C": [],
+                              "CM_DIS_04_C": ["close_mcl"],
+                              "CM_DIS_05_C": []
+                              },
+                "03_CM_REC": {
+                              "CM_REC_BV_01_C": ["send_data", "close_dc", "close_mcl", "con_mcl", "recon_dc"], #TODO: Failed with Invalid MDL configuration
+                              "CM_REC_BV_02_C": ["send_data", "send_data"],
+#                              "CM_REC_BV_03_C": ["send_data", "bt_down", "bt_up", "con_mcl", "recon_dc", "send_data"], #TODO: Failed with Invalid MDL configuration
+                              "CM_REC_BV_04_C": ["send_data", "bt_down", "bt_up", "send_data"],
+                              "CM_REC_BV_05_C": ["send_data", "close_dc", "recon_dc"], #TODO: Failed with Invalid MDL configuration
+                              "CM_REC_BV_06_C": ["send_data", "send_data"]
+                              },
+                "04_CM_DEL": {
+                              "CM_DEL_BV_01_C": ["del_dc"],
+                              "CM_DEL_BV_02_C": [],
+                              "CM_DEL_BV_03_C": ["del_all"],
+                              "CM_DEL_BV_04_C": [],
+                              },
+                "05_CM_ABT": {
+                              "CM_ABT_BV_01_C": ["con_mcl", "con_dc", "abort_dc"], #TODO: Fix it
+                              "CM_ABT_BV_02_C": [],
+                              "CM_ABT_BV_03_C": [],
+                              },
+                "06_ERR": {
+                           "ERR_BI_01C": [],
+                           "ERR_BI_02C": ["con_dc"],
+                           "ERR_BI_03C": [],
+                           "ERR_BI_04C": ["con_dc"],
+                           "ERR_BI_05C": [],
+                           "ERR_BI_06C": ["con_dc"],
+                           "ERR_BI_07C": [],
+                           "ERR_BI_08C": ["con_dc"],
+                           "ERR_BI_09C": [],
+                           "ERR_BI_10C": [],
+                           "ERR_BI_11C": ["con_dc"],
+                           "ERR_BI_12C": ["con_dc"],
+                           "ERR_BI_13C": [],
+                           "ERR_BI_14C": [],
+                           "ERR_BI_15C": ["con_dc"],
+#                           "ERR_BI_16C": [], #TODO: How implement this test
+                           "ERR_BI_17C": ["con_dc"],
+                           "ERR_BI_18C": ["con_dc"],
+                           "ERR_BI_19C": [],
+                           "ERR_BI_20C": ["enable_csp", "csp_cap"],
+                           },
+                "07_INV": {
+                           "INV_BI_01_C": ["con_dc"],
+                           "INV_BI_02_C": ["con_dc"],
+                           "INV_BI_03_C": ["con_mcl", "con_dc"], #TODO: Failed with Invalid MDL configuration
+                           "INV_BI_04_C": [],
+                           "INV_BI_05_C": [],
+                           "INV_BI_06_C": [],
+                           "INV_BI_07_C": [],
+                           },
+                "08_CS_I": {
+                            "CS_I_BV_01_I": ["enable_csp", "csp_cap", "csp_set"],
+                            "CS_I_BV_02_I": ["enable_csp", "csp_cap", "csp_seti"],
+                            "CS_I_BV_03_I": ["enable_csp", "csp_cap", "csp_set"],
+                            "CS_I_BV_04_I": ["enable_csp", "csp_cap", "csp_set"],
+                            },
+                "09_CS_R": {
+                            "CS_R_BV_01_I": ["enable_csp"],
+                            "CS_R_BV_02_I": ["enable_csp"],
+                            "CS_R_BV_03_I": ["enable_csp"],
+                            "CS_R_BV_04_I": ["enable_csp", "send_data"],
+                            },
+                "10_CS_ERR": {
+                              "CS_ERR_BI_01_C": ["enable_csp"], #TODO: Fix this test
+                              "CS_ERR_BI_02_C": ["enable_csp"],
+                              "CS_ERR_BI_03_C": ["enable_csp"],
+                              "CS_ERR_BI_04_C": ["enable_csp"],
+                              },
+                "11_QUIT": {
+                            "QUIT_01_C": ["exit"],
+                            },
+            }
+
+
+class TestSuite:
+    current_suite_index = 0
+    current_suite = None
+    current_test_commands = None
+    current_command = None
+    current_suite_index = 0
+    current_suite_keys = []
+    current_test_index = 0
+    current_command_index = 0
+
+    def __init__(self, suites):
+        self.suites = suites
+        self.suite_names_dict = suites.keys()
+        self.suite_names_dict.sort()
+        self.current_suite = self.suites[self.suite_names_dict[self.current_suite_index]]
+
+        self.current_suite_keys = self.current_suite.keys()
+        self.current_suite_keys.sort()
+
+        self.current_test_commands = self.current_suite[self.current_suite_keys[self.current_test_index]]
+        if len(self.current_test_commands) > 0:
+            self.current_command = self.current_test_commands[self.current_command_index]
+        else:
+            self.next_command()
+
+
+    def get_current_command(self):
+        return [self.current_command]
+
+
+    def next_command(self):
+        has_next_command = False
+
+        while has_next_command == False:
+            self.current_command_index += 1
+
+            if self.current_command_index >= len(self.current_test_commands):
+                self.current_test_index += 1
+                self.current_command_index = 0
+                if self.current_test_index >= len(self.current_suite_keys):
+                    #Go to next suite
+                    self.current_test_index = 0
+                    self.current_suite_index += 1
+                    if self.current_suite_index >= len(self.suite_names_dict):
+                        print "Done"
+                        exit()
+
+                    self.current_suite = self.suites[self.suite_names_dict[self.current_suite_index]]
+                    self.current_suite_keys = self.current_suite.keys()
+                    self.current_suite_keys.sort()
+
+                self.current_test_commands = self.current_suite[self.current_suite_keys[self.current_test_index]]
+
+                self.current_suite = self.suites[self.suite_names_dict[self.current_suite_index]]
+                self.current_suite_keys = self.current_suite.keys()
+                self.current_suite_keys.sort()
+
+            if self.current_command_index < len(self.current_test_commands):
+                has_next_command = True
+
+        self.current_command = self.current_test_commands[self.current_command_index]
+
+
+    def command_info(self):
+        print '\n===> Suite: "%s", Test: "%s", Command: "%s"' % (
+            self.suite_names_dict[self.current_suite_index][3:], self.current_suite_keys[self.current_test_index], self.current_command)
